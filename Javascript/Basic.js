22e229bfa38c1e4e1a047a59587e25c3a8dad07c
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-06-10 18:30:23 +0530

    Basic js started

---
+++ Javascript/Basic.js
@@ -0,0 +1,44 @@
+//  Variables  --
+// var a = 10 ;
+// let b = 10 ;
+// const c = 10 ;
+
+//  DataTypes -- String , number , boolean , undefined, null , symbol , NAN
+
+// let a = 'Saurabh';
+
+// a = 12.5
+
+// a = true
+
+// console.log(typeof(a));
+
+// undefined vs null
+
+// undefined and null bith are used for absence of value,
+// Intentially if we want empty value we have to use null , and undefined is used for unintentional empty value may be due to mistake
+
+// let a = null ;
+// console.log(a)
+
+// NAN
+
+// console.log(10/"a") // Not a number
+
+//operators
+// == vs ===
+
+// let a = 10;
+// let b = 10;
+
+// console.log(a == b); // true
+// console.log(a === b); // true
+
+let a = 10;
+let b = "10";
+
+console.log(a == b); // true
+console.log(a === b); // false
+// == firstly checks the dataType if it is diffrent it typecast to sametype and then compare the value 
+// == it compares the datatype if they are not same it will return false, if they are same it will check the value 
+
