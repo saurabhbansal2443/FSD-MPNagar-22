d212141c681acde391239f749ebb5a84bc3f4400
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-06-15 19:05:17 +0530

    Var complete

--- Javascript/Variables.js
+++ Javascript/Variables.js
@@ -19,11 +19,74 @@
 
 // var is hoisted , let and const are not hoisted
 
-console.log(a);
-var a = 10;
-console.log(a);
-one();
-function one() {
-  console.log("One called");
+// console.log(a);
+// var a = 10;
+// console.log(a);
+// one();
+// function one() {
+//   console.log(a);
+// }
+// one();
+
+// one();
+// console.log(a);
+// var a = 10;
+// console.log(a);
+// function one() {
+//   console.log(a);
+// }
+// one();
+
+// one();
+// console.log(a);
+// var a = 10;
+// var b = 199 ;
+// console.log(a);
+// function one() {
+//   console.log(a);
+//   var a = 99;
+//   console.log(a);
+// }
+// one();
+
+// one();
+// console.log(a);
+// var a = 10;
+// var b = 199;
+// console.log(a);
+// function one() {
+//   console.log(a);
+//   var a = 99;
+//   console.log(b);
+//   b = 200 ;
+// }
+// console.log(b);
+// one();
+
+// one();
+// console.log(a);
+// var a = 10;
+// console.log(b);
+// var b = 199;
+// console.log(a);
+// console.log(b);
+// function one() {
+//   console.log(a);
+//   var a = 99;
+//   console.log(b);
+//   b = 200;
+// }
+// console.log(b);
+// one();
+
+//  var is function scoped and let and const are block scoped
+// var let and const are function scoped because
+//  function is also a block and let and const is block scoped 
+
+function abc() {
+  let a = 10;
+  console.log(a);
 }
-one();
+
+abc();
+console.log(a);
