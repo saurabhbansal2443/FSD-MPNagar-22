a44ed661ce1ebf703912d39fc757e99b48e364e8
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-07-15 19:06:15 +0530

    Edit task

--- Javascript/DOM/TodoList/script.js
+++ Javascript/DOM/TodoList/script.js
@@ -14,7 +14,11 @@ addButton.addEventListener("click", function () {
   const taskBox = document.createElement("div");
   taskBox.classList.add("task");
   taskBox.innerHTML = `
-        <p>${currentTask.trim()}</p>
+        <p contentEditable="false" >${currentTask.trim()}</p>
+        <div>
+        <div id='edit'>
+        <svg   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M12.8995 6.85453L17.1421 11.0972L7.24264 20.9967H3V16.754L12.8995 6.85453ZM14.3137 5.44032L16.435 3.319C16.8256 2.92848 17.4587 2.92848 17.8492 3.319L20.6777 6.14743C21.0682 6.53795 21.0682 7.17112 20.6777 7.56164L18.5563 9.68296L14.3137 5.44032Z"></path></svg>
+        <div>
         <svg
         id='delete'
           xmlns="http://www.w3.org/2000/svg"
@@ -26,12 +30,24 @@ addButton.addEventListener("click", function () {
           <path
             d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"
           ></path>
-        </svg>`;
+        </svg>
+        </div>`;
 
   const deleteButton = taskBox.querySelector("#delete");
-
+  const editButton = taskBox.querySelector("#edit");
+  const taskTag = taskBox.querySelector("p");
   deleteButton.addEventListener("click", function () {
     taskContainer.removeChild(taskBox);
+  });
+
+  editButton.addEventListener("click", function () {
+    const contentEditableFlag = taskTag.getAttribute("contentEditable");
+    // console.log(contentEditableFlag);
+    if (taskTag.getAttribute("contentEditable") == true) {
+      taskTag.setAttribute("contentEditable", false);
+    } else {
+      taskTag.setAttribute("contentEditable", true);
+    }
   });
 
   taskContainer.appendChild(taskBox);
