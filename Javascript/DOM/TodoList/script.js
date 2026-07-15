284191214eab51c0319b92b9980178e314d10382
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-07-15 17:51:03 +0530

    Task 2 complete of todo list

---
+++ Javascript/DOM/TodoList/script.js
@@ -0,0 +1,6 @@
+const inputTag = document.getElementById("input");
+const addButton = document.getElementById("btn");
+
+addButton.addEventListener("click", function () {
+  let currentTask = inputTag.value;
+});
