c5434e7b157a6b0f4a143e5b7e2ce8857683c6e2
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-07-17 17:39:18 +0530

    Task 4 completed

--- Javascript/DOM/TodoList/script.js
+++ Javascript/DOM/TodoList/script.js
@@ -16,9 +16,9 @@ addButton.addEventListener("click", function () {
   taskBox.innerHTML = `
         <p contentEditable="false" >${currentTask.trim()}</p>
         <div>
-        <div id='edit'>
-        <svg   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M12.8995 6.85453L17.1421 11.0972L7.24264 20.9967H3V16.754L12.8995 6.85453ZM14.3137 5.44032L16.435 3.319C16.8256 2.92848 17.4587 2.92848 17.8492 3.319L20.6777 6.14743C21.0682 6.53795 21.0682 7.17112 20.6777 7.56164L18.5563 9.68296L14.3137 5.44032Z"></path></svg>
-        <div>
+       
+        <svg  id='edit'  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M12.8995 6.85453L17.1421 11.0972L7.24264 20.9967H3V16.754L12.8995 6.85453ZM14.3137 5.44032L16.435 3.319C16.8256 2.92848 17.4587 2.92848 17.8492 3.319L20.6777 6.14743C21.0682 6.53795 21.0682 7.17112 20.6777 7.56164L18.5563 9.68296L14.3137 5.44032Z"></path></svg>
+      
         <svg
         id='delete'
           xmlns="http://www.w3.org/2000/svg"
@@ -39,15 +39,16 @@ addButton.addEventListener("click", function () {
   deleteButton.addEventListener("click", function () {
     taskContainer.removeChild(taskBox);
   });
-
+  let flag = false;
   editButton.addEventListener("click", function () {
-    const contentEditableFlag = taskTag.getAttribute("contentEditable");
-    // console.log(contentEditableFlag);
-    if (taskTag.getAttribute("contentEditable") == true) {
+    if (flag) {
       taskTag.setAttribute("contentEditable", false);
+      editButton.setAttribute("fill", "black");
     } else {
       taskTag.setAttribute("contentEditable", true);
+      editButton.setAttribute("fill", "red");
     }
+    flag = !flag;
   });
 
   taskContainer.appendChild(taskBox);
