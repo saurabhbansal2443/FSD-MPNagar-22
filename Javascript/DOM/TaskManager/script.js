be037402c5ace5fabe2fb5208c1913bd1abaccc4
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-07-22 18:52:08 +0530

    keyboard events started

--- Javascript/DOM/TaskManager/script.js
+++ Javascript/DOM/TaskManager/script.js
@@ -1,6 +1,15 @@
 const addButton = document.getElementById("add");
 const taskAdderContainer = document.querySelector(".taskAdder");
+const taskTextInput = document.querySelector(".taskText");
 
 addButton.addEventListener("click", function () {
   taskAdderContainer.classList.toggle("hide");
+});
+
+taskTextInput.addEventListener("keydown", function (event) {
+  if (event.key != "Enter") {
+    return;
+  }
+  const taskText = taskTextInput.value;
+  console.log(taskText);
 });
