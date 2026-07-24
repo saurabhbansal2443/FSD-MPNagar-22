83243fa7d2dc224a406d21400fd06c769db91b5e
saurabhbansal2443 <saurabhbansal2443@gmail.com>
2026-07-24 17:46:06 +0530

    implemented Event Delgation for selection of color in task Adder

--- Javascript/DOM/TaskManager/script.js
+++ Javascript/DOM/TaskManager/script.js
@@ -1,6 +1,9 @@
 const addButton = document.getElementById("add");
 const taskAdderContainer = document.querySelector(".taskAdder");
 const taskTextInput = document.querySelector(".taskText");
+const taskAdderColorsContainer = document.querySelector(".priotityColors2");
+
+let selectedColor = "red";
 
 addButton.addEventListener("click", function () {
   taskAdderContainer.classList.toggle("hide");
@@ -12,4 +15,16 @@ taskTextInput.addEventListener("keydown", function (event) {
   }
   const taskText = taskTextInput.value;
   console.log(taskText);
+});
+
+taskAdderColorsContainer.addEventListener("click", function (event) {
+  const selectedElement = event.target;
+
+  if (selectedElement.classList[0] !== "color2") {
+    return;
+  }
+  const newSelectedColor = selectedElement.classList[1];
+  selectedColor = newSelectedColor;
+  selectedElement.classList.add("border");
+  console.log(newSelectedColor);
 });
